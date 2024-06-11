1170ab0719e8adcb3a449b3b3867aa3cd6f99696
JanaBan99 <95229070+JanaBan99@users.noreply.github.com>
2024-06-11 23:06:52 +0530

    Create transmitter_100bits.py

---
+++ transmitter_100bits.py
@@ -0,0 +1,27 @@
+import machine
+import utime
+
+# Define GPIO pins
+led_pin = machine.Pin(16, machine.Pin.OUT)
+
+data_word = '1010110010101100110101001100101011010011010110010101001101010100110101100101101001101010010110101'  # 99-bit data word to send
+initial_word = '1111100000'  # Initial 10-bit word to send before data_word
+
+# Function to send the data word
+def send_data_word(initial_word, data_word):
+    for bit in initial_word:  # Transmit initial 10 bits
+        led_pin.value(int(bit))  # Set the LED state based on the bit
+        print(bit, end='')  # Print the transmitting bit
+        utime.sleep_ms(1000)  # Wait for 1000 milliseconds (1 second)
+    print()
+    utime.sleep_ms(25)
+    
+    for bit in data_word:  # Transmit the 99-bit data word
+        led_pin.value(int(bit))  # Set the LED state based on the bit
+        print(bit, end='')  # Print the transmitting bit
+        utime.sleep_ms(1000)  # Wait for 1000 milliseconds (1 second)
+
+send_data_word(initial_word, data_word)
+led_pin.value(0)  # Turn off the LED
+
+
