8c7b0c16c08fb9902c39c58e67b39728eeb3517a
JanaBan99 <95229070+JanaBan99@users.noreply.github.com>
2024-06-11 23:18:57 +0530

    Create wifi_connect_receiver.py

---
+++ wifi_connect_receiver.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import network
+
+
+def run_secondary_script():
+    import receiver_final
+    receiver_final.run()
+
+wifi=network.WLAN(network.STA_IF)
+time.sleep(.5)
+wifi.active(True)
+time.sleep(.5)
+wifi.connect('Pixel_3a','jana99bandara')
+cnt=0
+while wifi.isconnected()== False:
+    print('Waiting..........')
+    time.sleep(1)
+    cnt=cnt+1
+
+wifiInfo=wifi.ifconfig()
+print(wifiInfo)
+ServerIp=wifiInfo[0]
+print(ServerIp)
+ServerPort=2223
+bufferSize=1024
+UDPServer=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+UDPServer.bind((ServerIp,ServerPort))
+while True: 
+    cmd, address =UDPServer.recvfrom(bufferSize)
+    cmdDecoded=cmd.decode('utf-8')
+    print(cmdDecoded)
+    print('From: ', str(address[0]))
+    if cmdDecoded == 'start':
+        run_secondary_script()
